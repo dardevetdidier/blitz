3416ae59f5e5f0a48c563b7de532f2034527b66c
Didier Dardevet <didier.dardevet@gmail.com>
2021-05-07 15:36:39 +0200

    first commit

---
+++ blitz.py
@@ -0,0 +1,5 @@
+# TODO 1 : Créer classe joueurs (inputs) -> fichier json : liste de dictionnaires (1 joueur / dict)
+
+# TODO 2 : Créer paire de joueurs -> système suisse
+
+# TODO 3 : Créer résultats avec (inputs) -> fichier json ?
