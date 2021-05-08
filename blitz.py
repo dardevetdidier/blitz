0773f77db21fe0bd4010ac8e4a6e9b50b626c310
Didier Dardevet <didier.dardevet@gmail.com>
2021-05-08 08:28:33 +0200

    Ajout joueur fichier json

--- blitz.py
+++ blitz.py
@@ -1,4 +1,32 @@
-# TODO 1 : Créer classe joueurs (inputs) -> fichier json : liste de dictionnaires (1 joueur / dict)
+import json
+
+class Joueur:
+
+    def __init__(self):
+        self.nom = input("Nom : ")
+        self.prenom = input("Prénom : ")
+        self.date_naissance = input("Date de naissance : ")
+        self.sexe = input("Sexe : ")
+        self.classement = input("Classement : ")
+
+    def ajouter_joueur(self):
+        with open("joueurs.json", "r", encoding="utf-8") as f:
+            joueurs = json.load(f)
+        joueur = {
+            'nom': self.nom,
+            'prenom': self.prenom,
+            'date de naissance': self.date_naissance,
+            'sexe': self.sexe,
+            'classement': self.classement
+        }
+        joueurs.append(joueur)
+
+        with open('joueurs.json', "w", encoding="utf-8") as f:
+            json.dump(joueurs, f, indent=4, ensure_ascii=False)
+
+
+joueur1 = Joueur()
+joueur1.ajouter_joueur()
 
 # TODO 2 : Créer paire de joueurs -> système suisse
 
