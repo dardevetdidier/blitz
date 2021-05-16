10a44d8464e51596f70ef91b433097d2268bad46
Didier Dardevet <didier.dardevet@gmail.com>
2021-05-16 18:45:29 +0200

    add method to create pairs sorted by rank

--- test.py
+++ test.py
@@ -59,32 +59,33 @@
 # sort list of players by rank
 import json
 import pprint
+from random import randint
 
 
-def sort_by_rank():
-    with open("players.json", "r", encoding="utf-8") as f:
-        players = json.load(f)
-    rank_sorted_list = sorted(players, key=lambda k: k['rank'])
-    return rank_sorted_list
+# def sort_by_rank():
+#     """Sorts list of players by rank and returns a new list of players"""
+#     # with open("players.json", "r", encoding="utf-8") as f:
+#     #     players = json.load(f)
+#     rank_sorted_list = sorted(generates_players(), key=lambda k: k['rank'])
+#     return rank_sorted_list
 
 
 def generates_players():
     """User enters informations of 8 players. returns list of 8 players"""
     players = []
-    for i in range(1, 9):
-        print(f"\nEntrez les informations du joueur {i} :\n ")
-        player_id = i
-        name = input("Nom : ")
-        first_name = input("Prénom : ")
-        birth = input("Date de naissance : ")
-        sex = input("Sexe : ")
-        rank = int(input("Classement : "))
+    for player in range(1, 9):
+        print(f"\nEntrez les informations du joueur {player} :\n ")
+        player_id = player
+        name = f"joueur "  # input("Nom : ")
+        first_name = str(player)  # input("Prénom : ")
+        birth = "01/01/2000"  # input("Date de naissance : ")
+        sex = "m"  # input("Sexe : ")
+        rank = randint(1, 50)  # int(input("Classement : "))
 
-        # TODO : FAUT-IL METTRE LA SUITE DANS MAIN()?
         player = Player(player_id, name, first_name, birth, sex, rank)
         players.append(player.player)
-    with open('players.json', "w", encoding='utf-8') as f:
-        json.dump(players, f, indent=4, ensure_ascii=False)
+    # with open('players.json', "w", encoding='utf-8') as f:
+    #     json.dump(players, f, indent=4, ensure_ascii=False)
 
     return players
 
@@ -97,12 +98,13 @@ def launch_tournament():
     time_control = input("'Bullet', 'Blitz' ou 'Coup rapide'? : ")
     notes = input("Remarques du directeur : ")
 
-    # TODO : FAUT-IL METTRE LA SUITE DANS MAIN()
     players = generates_players()
     tournament = Tournament(name, location, date, players, time_control, notes)
-    tournament.display_tournament_infos()
-    with open('tournament.json', "w") as f:
-        json.dump(tournament.tournaments, f, indent=4, ensure_ascii=False)
+    # tournament.pairs_by_rank()
+    # pprint.pprint(tournament.pairs_by_rank())
+    # tournament.display_tournament_infos()
+    # with open('tournament.json', "w") as f:
+    #     json.dump(tournament.tournaments, f, indent=4, ensure_ascii=False)
     return tournament
 
 
@@ -126,6 +128,18 @@ class Tournament:
             'time_control': self.time_control,
             'notes': self.notes
         }
+
+    def pairs_by_rank(self):
+        """Sorts list of players by rank and creates 4 pairs. returns a list of 4 lists of 2 players"""
+        rank_sorted_list = sorted(self.players, key=lambda k: k['rank'])
+        pairs_sort_by_rank = []
+        for index in range(0, 4):
+            pair_sort_by_rank = []
+            player1 = rank_sorted_list[index]
+            player2 = rank_sorted_list[index + 4]
+            pair_sort_by_rank.extend([player1, player2])
+            pairs_sort_by_rank.append(pair_sort_by_rank)
+        return pairs_sort_by_rank
 
     def display_tournament_infos(self):  # -> Vue
         print(self.tournaments)
@@ -162,40 +176,40 @@ class Player:
             json.dump(players, f, indent=4, ensure_ascii=False)
 
 
-class FirstRound:
-    def __init__(self, name, start_time):
-        self.round_name = name
-        self.start_time = start_time
-        self.end_time = None  # changes when enters results
-        self.pairs = StartingAllPairs.starting_pairs_list
+# class FirstRound:
+#     def __init__(self, name, start_time):
+#         self.round_name = name
+#         self.start_time = start_time
+#         self.end_time = None  # changes when enters results
+#         self.pairs = StartingAllPairs.starting_pairs_list
 
-
-class StartingAllPairs:  # Methode round ou tournoi ou mettre round dans tournoi
-    starting_pairs_list = []
-
-    def __init__(self):
-        for index in range(0, 4):
-            self.starting_pair = []
-            self.player1 = sort_by_rank()[index]
-            self.player2 = sort_by_rank()[index + 4]
-            self.starting_pair.extend([self.player1, self.player2])
-            StartingAllPairs.starting_pairs_list.append(self.starting_pair)
+#
+# class StartingAllPairs:  # Methode round ou tournoi ou mettre round dans tournoi
+#     starting_pairs_list = []
+#
+#     def __init__(self):
+#         for index in range(0, 4):
+#             self.starting_pair = []
+#             self.player1 = sort_by_rank()[index]
+#             self.player2 = sort_by_rank()[index + 4]
+#             self.starting_pair.extend([self.player1, self.player2])
+#             StartingAllPairs.starting_pairs_list.append(self.starting_pair)
 
 
 # Cette classe ne sert à rien !!!! cf def enter_results()
-class Results:
-    def __init__(self, player1_score, player2_score):
-        for index in range(0, 1):
-            self.player1 = sort_by_rank()[index]
-            self.player2 = sort_by_rank()[index + 4]
-        self.player1_score = player1_score
-        self.player2_score = player2_score
-
-    def save_results(self):
-        scores = ([f"{self.player1['name']} {self.player1['first_name']}", self.player1_score],
-                  [f"{self.player2['name']} {self.player2['first_name']}", self.player2_score])
-
-        return scores
+# class Results:
+#     def __init__(self, player1_score, player2_score):
+#         for index in range(0, 1):
+#             self.player1 = sort_by_rank()[index]
+#             self.player2 = sort_by_rank()[index + 4]
+#         self.player1_score = player1_score
+#         self.player2_score = player2_score
+#
+#     def save_results(self):
+#         scores = ([f"{self.player1['name']} {self.player1['first_name']}", self.player1_score],
+#                   [f"{self.player2['name']} {self.player2['first_name']}", self.player2_score])
+#
+#         return scores
 
 
 def enter_results(player1, player2):
@@ -213,8 +227,9 @@ def enter_results(player1, player2):
     return result
 
 
-# launch_tournament()
-pairs = StartingAllPairs().starting_pairs_list
+pairs = launch_tournament().pairs_by_rank()  # ---> objet tournament
+
+# pairs = StartingAllPairs().starting_pairs_list
 pprint.pprint(pairs, sort_dicts=False)
 
 results_list = []
